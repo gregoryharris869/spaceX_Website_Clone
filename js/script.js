0cfd9c8a4306865de3df36f1adf15681ccc26948
Gregory Harris <63122765+gregoryharris869@users.noreply.github.com>
2022-08-21 14:45:57 -0500

    More CSS and javaScript

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const btn = document.getElementById("menu-btn");
 const overlay = document.getElementById("overlay");
 const menu = document.getElementById("mobile-menu");
+const counters = document.querySelectorAll(".counter");
 
 btn.addEventListener("click", navToggle);
 
@@ -10,3 +11,30 @@ function navToggle() {
   document.body.classList.toggle("stop-scrolling");
   menu.classList.toggle("show-menu");
 }
+
+function countUp() {
+  counters.forEach((counter) => {
+    counter.innerText = "0";
+
+    const updateCounter = () => {
+      // Get count target
+      const target = +counter.getAttribute("data-target");
+      // Get current counter value
+      const c = +counter.innerText;
+      // Create an increment counter
+      const increment = target / 100;
+      // If counter is less than target, add increment
+      if (c < target) {
+        // Round up and set counter value
+        counter.innerText = `${Math.ceil(c + increment)}`;
+
+        setTimeout(updateCounter, 75);
+      } else {
+        counter.innerText = target;
+      }
+    };
+    updateCounter();
+  });
+}
+
+countUp();
