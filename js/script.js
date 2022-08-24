7d798aaf64c4ebcd8284c7b81359accacb56e3cc
Gregory Harris <63122765+gregoryharris869@users.noreply.github.com>
2022-08-24 17:38:30 -0500

    Finished

--- js/script.js
+++ js/script.js
@@ -2,14 +2,27 @@ const btn = document.getElementById("menu-btn");
 const overlay = document.getElementById("overlay");
 const menu = document.getElementById("mobile-menu");
 const counters = document.querySelectorAll(".counter");
+let scrollStarted = false;
 
 btn.addEventListener("click", navToggle);
+document.addEventListener("scroll", scrollPage);
 
 function navToggle() {
   btn.classList.toggle("open");
   overlay.classList.toggle("overlay-show");
   document.body.classList.toggle("stop-scrolling");
   menu.classList.toggle("show-menu");
+}
+
+function scrollPage() {
+  const scrollPos = window.scrollY;
+  if (scrollPos > 100 && !scrollStarted) {
+    countUp();
+    scrollStarted = true;
+  } else if (scrollPos < 100 && scrollStarted) {
+    reset();
+    scrollStarted = false;
+  }
 }
 
 function countUp() {
@@ -37,4 +50,6 @@ function countUp() {
   });
 }
 
-countUp();
+function reset() {
+  counters.forEach((counter) => (counter.innerHTML = "0"));
+}
